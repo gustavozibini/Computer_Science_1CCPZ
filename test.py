5bcd7506c7394daf3979a3d0edfe8abb66e13ef0
gustavozibini <gustavo_belizario@hotmail.com>
2026-03-20 20:38:58 -0300

    update

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-#print("hello world!!")
-'''print("hello world!!")'''
-check1 = 9
-check2 = 7.5
-check3 = 8
+# #print("hello world!!")
+# '''print("hello world!!")'''
+# check1 = 9
+# check2 = 7.5
+# check3 = 8
+#
+# media = (check1 + check2 + check3)/3
+# print(media)
+# # print((check1 + check2 + check3)/3)
+# print(f"Média = {media:.2f}")
+#
+#
+# nome = input("Digite seu nome: ")
+# idade = input("Digite sua idade: ")
+# print(nome, idade)
 
-media = (check1 + check2 + check3)/3
-print(media)
-#print((check1 + check2 + check3)/3)
-print(f"Média = {media:.2f}")
