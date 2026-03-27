76786c4725f1186732cf605f1645bfaa9fd06e12
gustavozibini <gustavo_belizario@hotmail.com>
2026-03-27 20:45:10 -0300

    update

---
+++ CP1.py
@@ -0,0 +1,28 @@
+# # CP1
+# p_name = input("Digite o nome do produto: ")
+# p_val = int(input("Digite o valor: "))
+# p_qty = int(input("Digite a quantidade: "))
+# p_pct = float(input("Digite a porcentagem de desconto: "))
+#
+# v_total = p_val * p_qty
+# v_off = (v_total * p_pct) / 100
+# v_final = v_total - v_off
+#
+# print(f"Produto: {p_name}\n"
+#       f"Valor bruto: R${v_total:.2f}\n"
+#       f"Desconto: R${v_off:.2f}\n"
+#       f"Valor final: R${v_final:.2f}")
+#
+# e_name = input("Digite o nome do colaborador: ")                   #nome do colaborador
+# h_val = int(input("Digite o valor da hora: "))                 #valor hora
+# e_h_qty = int(input("Digite a quantidade de horas: "))         #quantidade de horas
+# e_bon = float(input("Digite o bonus: "))                       #bonus fixo
+# v_dsc = int(input("Digite o valor do desconto: "))             #desconto total
+#
+# s_total = h_val * e_h_qty + e_bon
+# s_off = s_total - v_dsc
+#
+#
+# print(f"Colaborador: {e_name}\n"
+#       f"Salario bruto: R${s_total:.2f}\n"
+#       f"Salario liquido: R${s_off:.2f}\n")
--- test.py
+++ test.py
@@ -10,7 +10,16 @@
 # print(f"Média = {media:.2f}")
 #
 #
-# nome = input("Digite seu nome: ")
-# idade = input("Digite sua idade: ")
-# print(nome, idade)
+p_name = input("Digite o nome do produto: ")
+p_val = int(input("Digite o valor: "))
+p_qty = int(input("Digite a quantidade: "))
+p_pct = float(input("Digite a porcentagem de desconto: "))
 
+v_total = p_val * p_qty
+v_off = (v_total * p_pct) / 100
+v_final = v_total - v_off
+
+print(f"Produto: {p_name}\n"
+      f"Valor bruto: R${v_total:.2f}\n"
+      f"Desconto: R${v_off:.2f}\n"
+      f"Valor final: R${v_final:.2f}")
