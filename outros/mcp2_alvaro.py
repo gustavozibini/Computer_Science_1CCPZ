5f32f0aa2ea937f457570a7c8d37cce4ccb6f72f
gustavozibini <gustavo_belizario@hotmail.com>
2026-08-24 21:01:07 -0300

    .

--- outros/mcp2_alvaro.py
+++ outros/mcp2_alvaro.py
@@ -1,57 +1,67 @@
+import random
 
-
-numeros = [6, 42, 87, 15, 73, 29, 95, 50, 11, 68, 3, 80, 24, 61, 37, 98, 54, 18, 76, 32]
-
+numeros = random.sample(range(1, 300), 200)
 
 
 # SELECTION_SORT
 def selection_sort(lista):
+    comparacoes = 0
     trocas = 0
     n = len(lista)
     for i in range(n):
         menor = i
-        for j in range( i + 1,n):
-           if lista[j] < lista[menor]:
-              menor = j
-        lista[i], lista[menor] = lista[menor], lista[i]
-        trocas+=1
-    return lista, trocas
-
-
+        for j in range( i + 1, n):
+            comparacoes += 1
+            if lista[j] < lista[menor]:
+                menor = j
+        if menor != i:
+            lista[i], lista[menor] = lista[menor], lista[i]
+            trocas += 1
+    return lista, comparacoes, trocas
 
 def insertion_sort(lista):
+    comparacoes = 0
     trocas = 0
     for i in range(1, len(lista)):
         atual = lista[i]
         j = i - 1
-        while j >= 0 and lista[j] > atual:
-            lista[j+1] = lista[j]
-            j = j - 1
-            trocas = trocas + 1
-        lista[j+1] = atual
-
-    return lista, trocas
+        while j >= 0:
+            comparacoes += 1
+            if lista[j] > atual:
+                lista[j + 1] = lista[j]
+                j -= 1
+                trocas += 1
+            else:
+                break
+        lista[j + 1] = atual
+    return lista, comparacoes, trocas
 
 
 def bubble_sort(lista):
-    trocas = 0
     n = len(lista)
+    comparacoes = 0
+    trocas = 0
     for i in range(n):
-        for j in range(n - i - 1):
+        for j in range(n - 1 - i):
+            comparacoes += 1
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
-                trocas +=1
-    return lista, trocas
+                trocas += 1
+    return lista, comparacoes, trocas
+
 
 print()
-lista, trocas = bubble_sort(numeros.copy())
+lista, comparacoes, trocas = bubble_sort(numeros.copy())
 print("Bubble sort: ", lista)
+print("Trocas comparacoes: ", comparacoes)
 print("Trocas Realizadas: ", trocas)
 print()
-lista, trocas = insertion_sort(numeros.copy())
+lista, comparacoes, trocas = insertion_sort(numeros.copy())
 print("Insertion sort: ", lista)
+print("Trocas comparacoes: ", comparacoes)
 print("Trocas Realizadas: ", trocas)
 print()
-lista, trocas = selection_sort(numeros.copy())
+lista, comparacoes, trocas = selection_sort(numeros.copy())
 print("Selection sort: ", lista)
+print("Trocas comparacoes: ", comparacoes)
 print("Trocas Realizadas: ", trocas)
